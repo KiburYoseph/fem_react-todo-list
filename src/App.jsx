d8021f1482a8f052640b2e879453d0bf9139e5bc
KiburYoseph <kiburstgr@gmail.com>
2023-08-22 05:06:04 +0300

    Light mode CSS done

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,25 @@ const App = () => {
         <section className='content'>
           <h1>TODO</h1>
           <form>
-            <input placeholder='Create a new todo'/>
+            <input placeholder='Create a new todo'></input>
+            <button className='inputButton'></button>
           </form>
           <div className='list'>
-            <ul>
-              <li><div className='listCircle'></div> Complete Online Javascript Course</li>
-              <li><div className='listCircle'></div> Jog around the park 3x</li>
-              <li><div className='listCircle'></div> 10 minutes meditation</li>
-              <li><div className='listCircle'></div> Read for 1 hour</li>
-            </ul>
+              <div className='listItem'><div className='listCircle'></div> Complete Online Javascript Course</div>
+              <div className='listItem'><div className='listCircle'></div> Jog around the park 3x</div>
+              <div className='listItem'><div className='listCircle'></div> 10 minutes meditation</div>
+              <div className='listItem'><div className='listCircle'></div> Read for 1 hour</div>
+            <section className='info'>
+              <p>4 items left</p>
+              <div className='interactions'>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+              </div>
+                <button className='clearComp'>Clear Completed</button>
+            </section>
           </div>
-          <section className='info'>
-            <p>4 items left</p>
-            <div className='interactions'>
-              <button>All</button>
-              <button>Active</button>
-              <button>Completed</button>
-              <button>Clear Completed</button>
-            </div>
-          </section>
+          
         </section>
 
       </main>
